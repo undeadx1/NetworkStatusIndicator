93a9f21dfb7c876d54fd7fa6f3e3870cde53a350
GeirDev <undeadx1@gmail.com>
2023-04-18 01:50:41 +0900

    Network Status Indicator
    Network Status Indicator
    A simple and customizable network status indicator for Unity. The script displays a series of bars representing the quality of the network connection, where the number of visible bars changes according to the current network latency. No additional image files are needed, as the indicator is generated programmatically.
    
    Features
    Real-time network latency detection.
    Customizable bar colors and dimensions.
    Easy to integrate with any Unity project.
    No additional image files required.
    Installation
    Download the NetworkStatusIndicator.cs script from this repository.
    Import the script into your Unity project by placing it in your Assets folder.
    Create a new GameObject in your scene and attach the NetworkStatusIndicator script to it.
    Add a UI Image component to the GameObject and assign it to the currentStatusIcon field in the NetworkStatusIndicator script.
    Usage
    After installing the script and setting up the GameObject, the network status indicator will automatically update the bars' visibility and colors based on the current network latency. The indicator sends a request to a specified URL every 5 seconds to measure the latency. You can replace the example URL (http://www.google.com) in the CheckNetworkStatus

---
+++ NetworkStatusIndicator.cs
@@ -0,0 +1,89 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Networking;
+
+namespace GeirDev
+{
+    public class NetworkStatusIndicator : MonoBehaviour
+    {
+        public Image currentStatusIcon;
+        private Texture2D statusTexture;
+
+        private void Start()
+        {
+            StartCoroutine(CheckNetworkStatus());
+        }
+
+        private IEnumerator CheckNetworkStatus()
+        {
+            while (true)
+            {
+                using (UnityWebRequest www = UnityWebRequest.Get("http://www.google.com"))
+                {
+                    float startTime = Time.time;
+                    yield return www.SendWebRequest();
+                    float ping = Time.time - startTime;
+
+                    float sensitivity = GetNetworkSensitivity(ping);
+                    SetStatusTextureSensitivity(sensitivity);
+
+                    yield return new WaitForSeconds(5);
+                }
+            }
+        }
+
+        private float GetNetworkSensitivity(float ping)
+        {
+            float maxPing = 300;
+            float minPing = 0;
+
+            return Mathf.Clamp01(1 - (ping - minPing) / (maxPing - minPing));
+        }
+
+        private void SetStatusTextureSensitivity(float sensitivity)
+        {
+            int width = 40;
+            int height = 100;
+            statusTexture = new Texture2D(width, height);
+
+            int numOfBars = 4;
+            int barWidth = (width - (numOfBars - 1)) / numOfBars;
+            int spacing = 1;
+            float maxHeightRatio = 0.8f;
+            float adjustedSensitivity = Mathf.Max((sensitivity - 0.05f) / (1 - 0.05f), 0);
+            Color darkGray = new Color(0.3f, 0.3f, 0.3f);
+
+            for (int y = 0; y < height; y++)
+            {
+                for (int x = 0; x < width; x++)
+                {
+                    int barIndex = x / (barWidth + spacing);
+                    bool inBarArea = x % (barWidth + spacing) < barWidth;
+                    float barHeightRatio = (barIndex + 1) / (float)numOfBars * maxHeightRatio;
+                    float barHeight = height * barHeightRatio;
+
+                    if (inBarArea && y < barHeight)
+                    {
+                        statusTexture.SetPixel(x, y, GetBarColor(barIndex, numOfBars, adjustedSensitivity, darkGray));
+                    }
+                    else
+                    {
+                        statusTexture.SetPixel(x, y, Color.clear);
+                    }
+                }
+            }
+
+            statusTexture.Apply();
+            currentStatusIcon.sprite = Sprite.Create(statusTexture, new Rect(0, 0, statusTexture.width, statusTexture.height), new Vector2(0.5f, 0.5f));
+        }
+
+        private Color GetBarColor(int barIndex, int numOfBars, float adjustedSensitivity, Color darkGray)
+        {
+            float barPercentage = barIndex / (float)(numOfBars - 1);
+            bool showBar = adjustedSensitivity >= barPercentage;
+            return showBar ? Color.Lerp(Color.red, Color.green, adjustedSensitivity) : darkGray;
+        }
+    }
+}
